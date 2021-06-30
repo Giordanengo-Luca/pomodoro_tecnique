01470f61336b7205a963293901ea37557b3087c8
Giordanengo-Luca <lucagiordanengo99@protonmail.com>
2021-06-30 16:02:58 +0200

    removed for cicles and used setInterval

--- pomodoro.js
+++ pomodoro.js
@@ -8,78 +8,76 @@ async function pomodoroTecniqueMain(){
   try {
     const settings = await getSettings();
     const bars = initProgressBars(settings);
-    await pomodoroTecnique(settings, bars);
-    notifyTaskComplete();
+    const status = {
+        cicles:0,
+        working:true
+    };
+    const timeline = setInterval(() => {
+        handleTime(timeline , settings, bars, status);
+    }, TIME_FACTOR);
   } catch (error) {
     console.log(error);
   }
 }
 
 function initProgressBars(settings){
-  const multibar = new cliProgress.MultiBar({
-    clearOnComplete: false,
-    hideCursor: true,
-    format: '{task} [{bar}] {percentage}% | {value}/{total}'
-
-  }, cliProgress.Presets.shades_grey);
-  const totalWorkBar = multibar.create(settings.totTime * 60, 0, {task:"Lavoro totale\t"});
-  const workBar = multibar.create(settings.workTime, 0, {task:"Sessione lavoro\t"});
-  const breakBar = multibar.create(settings.shortBreakTime, 0, {task:"Pausa corta\t"});
-  return {multibar, totalWorkBar, workBar, breakBar};
-}
-
-async function pomodoroTecnique(settings, bars){
   try {
-    let cicles = 0;
-    for(let totTime = 0; totTime < settings.totTime * 60;){
-      await simulateProgress(settings.workTime,
-      incr => {
-        bars.workBar.increment(incr);
-        bars.totalWorkBar.increment(incr);
-        totTime+=incr;
-      },
-      () => totTime == settings.totTime * 60,
-      () => {bars.workBar.update(0);});
+    const multibar = new cliProgress.MultiBar({
+      clearOnComplete: false,
+      hideCursor: true,
+      format: '{task} [{bar}] {percentage}% | {value}/{total}'
 
-      if(totTime == settings.totTime * 60)
-          break;
-      
-      await simulateProgress(cicles == settings.nCicles - 1? settings.longBreakTime : settings.shortBreakTime, incr => {
-        bars.breakBar.increment(incr);
-      }, () => false,
-      () => {
-        cicles = (cicles + 1) % settings.nCicles;
-      if(cicles != settings.nCicles - 1){
-        bars.breakBar.stop();
-        bars.breakBar.start(settings.shortBreakTime, 0, {task:"Pausa corta\t"});
-      } else {
-        bars.breakBar.stop();
-        bars.breakBar.start(settings.longBreakTime, 0, {task:"Pausa lunga\t"});
-      }
-      });
-    }
-    bars.multibar.stop();
-  } catch(error) {
+    }, cliProgress.Presets.shades_grey);
+    const totalWorkBar = multibar.create(settings.totTime * 60, 0, {task:"Lavoro totale\t"});
+    const workBar = multibar.create(settings.workTime, 0, {task:"Sessione lavoro\t"});
+    const breakBar = multibar.create(settings.shortBreakTime, 0, {task:"Pausa corta\t"});
+    return {multibar, totalWorkBar, workBar, breakBar};
+  } catch(error){
     throw error;
   }
 }
 
-async function simulateProgress(totTime, body, exitCond = () => false, finalOperation = () => {}, timeUnit = 1){
-  try {
-    for(let time = 0; time < totTime && !exitCond(); time+= timeUnit){
-      body(timeUnit);
-      await sleep(timeUnit * TIME_FACTOR);
-    }
-    finalOperation();
-  } catch (error) {
-    throw error;
-  }
+function handleTime(timeline, settings, bars, status){
+    try {
+        if (bars.totalWorkBar.value < settings.totTime * 60) {
+            return pomodoroTecnique(settings, bars, status);
+        }
+        bars.multibar.stop();
+        clearInterval(timeline);
+        setTimeout(notifyTaskComplete, 0);
+    } catch (error) {
+        throw error;
+    } 
 }
 
-function sleep(ms) {
-  return new Promise((resolve) => {
-    setTimeout(resolve, ms);
-  });
+function pomodoroTecnique(settings, bars, status){
+    try {
+        if(status.working) {
+            bars.workBar.increment();
+            bars.totalWorkBar.increment();
+            if(bars.workBar.value == bars.workBar.total){
+                status.working = false;
+                bars.workBar.update(0);
+            }
+            return;
+        } else {
+            const pauseTIme = status.cicles == settings.nCicles - 1? settings.longBreakTime : settings.shortBreakTime;
+            bars.breakBar.increment();
+            if(bars.breakBar.value == bars.breakBar.total){
+                status.cicles = (status.cicles + 1) % settings.nCicles;
+                status.working = true;
+                if(status.cicles != settings.nCicles - 1){
+                    bars.breakBar.stop();
+                    bars.breakBar.start(settings.shortBreakTime, 0, {task:"Pausa corta\t"});
+                } else {
+                    bars.breakBar.stop();
+                    bars.breakBar.start(settings.longBreakTime, 0, {task:"Pausa lunga\t"});
+                }
+            }
+        }
+    } catch(error) {
+        throw error;
+    }
 }
 
 function notifyTaskComplete(){
